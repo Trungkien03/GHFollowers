2ceb7423dd2945f3540da9b7a557b6d252ff6b69
Kien Nguyen <trungkiennguyen0310@gmail.com>
2025-11-29 13:14:12 +0700

    Initial Commit

---
+++ GHFollowersTests/GHFollowersTests.swift
@@ -0,0 +1,17 @@
+//
+//  GHFollowersTests.swift
+//  GHFollowersTests
+//
+//  Created by Nguyen Trung Kien on 29/11/25.
+//
+
+import Testing
+@testable import GHFollowers
+
+struct GHFollowersTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
